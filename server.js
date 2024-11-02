f1eea166819fe11e68daa8d61afdc10591c49100
ishara_wijerathne <damindishara74@gmail.com>
2024-11-02 19:35:17 +0530

    set up db connection

--- server.js
+++ server.js
@@ -1,1 +1,39 @@
-console.log("testging Done");
+const express = require('express');
+const app = express();
+
+const dotenv = require('dotenv')
+const mongoose = require('mongoose');
+dotenv.config({
+    path : './.env'
+})
+
+
+app.use(express.json())
+
+
+
+
+
+
+
+
+mongoose.connect(process.env.APP_MONGO_CON_SRT)
+    .then((connection) =>{
+        if(process.env.APP_ENV === "development")
+            {
+                console.log("mongodb connected successfully");  
+            }
+    }).catch((error) =>{
+            console.log(error.message);
+            
+    });
+
+
+app.listen(process.env.APP_PORT,() =>{
+
+    if(process.env.APP_ENV === "development")
+        {
+            console.log("server is running on port " + process.env.APP_PORT );  
+        }
+    
+})
